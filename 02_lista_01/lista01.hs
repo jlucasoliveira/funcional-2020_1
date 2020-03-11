50044fdc59e99ad885e42f23135c16bfabd5e4aa
Lucas Oliveira <lucasosilva68@gmail.com>
2020-03-11 23:56:34 +0000

    :construction: +2 questoes

--- 02_lista_01/lista01.hs
+++ 02_lista_01/lista01.hs
@@ -1,15 +1,17 @@
--- Para a questao 28
-import Data.Char
-import Data.List
+import Data.Char -- toUpper, toLower
+import Data.List -- sort
+
 -- Questao 1
 menorDeDois :: (Ord a) => a -> a -> a
-menorDeDois x y | x < y = x
+menorDeDois x y
+    | x < y = x
     | otherwise = y
 -- menorDeDois x y = min x y
 
 -- Questao 2
 menorDeTres :: (Ord a) => a -> a -> a -> a
-menorDeTres x y z | x < y && x < z = x
+menorDeTres x y z
+    | x < y && x < z = x
     | y < x && y < z = y
     | otherwise = z
 -- menorDeTres x y z = min x $ min y z
@@ -33,7 +35,8 @@ elemento n u = u !! n
 -- Questao 6
 pertence :: (Eq a) => a -> [a] -> Bool
 pertence n [] = False
-pertence n (x:xs) | x == n = True
+pertence n (x:xs)
+    | x == n = True
     | otherwise = pertence n xs
 -- pertence u x = elem x u
 
@@ -44,13 +47,15 @@ total (_:xs) = 1 + total xs
 
 -- Questao 8
 maior :: (Ord a) => [a] -> a
-maior (x:xs)| (length xs == 0 || (maior xs) < x) = x
+maior (x:xs)
+    | null xs || maior xs < x = x
     | otherwise = maior xs
 
 -- Questao 9
 frequencia :: (Eq a) => a -> [a] -> Int
 frequencia n [] = 0
-frequencia n (x:xs) | x == n = 1 + (frequencia n xs)
+frequencia n (x:xs)
+    | x == n = 1 + frequencia n xs
     |otherwise = frequencia n xs
 
 -- Questao 10
@@ -61,7 +66,8 @@ unico n u = frequencia n u == 1
 -- Questao 11
 maioresQue :: Int -> [Int] -> [Int]
 maioresQue n [] = []
-maioresQue n (x:xs) |  n < x = x:maioresQue n xs
+maioresQue n (x:xs)
+    |  n < x = x:maioresQue n xs
     | otherwise = maioresQue n xs
 
 -- Questao 12
@@ -79,7 +85,8 @@ corpo :: [a] -> [a]
 corpo [] = []
 corpo (x:[_]) = [x]
 corpo (x:xs) = x:corpo xs
--- corpo (x:xs) | (length xs) == 1 = [x]
+-- corpo (x:xs)
+    -- | (length xs) == 1 = [x]
     -- | otherwise = [x] ++ corpo xs
 -- corpo u = init u
 -- corpo u = take ((length u) - 1) u
@@ -91,6 +98,7 @@ unique [x] = [x]
 unique all@(x:xs)
     | unico x all = x:unique xs
     | otherwise = unique xs
+-- unique u = nub u
 
 -- Questao 16
 menores :: Int -> [Int] -> [Int]
@@ -129,12 +137,14 @@ uniao :: (Eq a) => [a] -> [a] -> [a]
 uniao u [] = u
 uniao [] x = x
 uniao u x = u ++ uniao' u x
+-- uniao u x = union u x
 
 -- Questao 22
 intersec :: (Eq a) => [a] -> [a] -> [a]
 intersec _ [] = []
 intersec [] _ = []
-intersec (x:xs) b | frequencia x b /= 0 = x:(intersec xs b)
+intersec (x:xs) b
+    | frequencia x b /= 0 = x:intersec xs b
     | otherwise = intersec xs b
 
 -- Questao 23
@@ -145,20 +155,28 @@ sequencia n m = [m + x | x <- [0..(n-1)]]
 -- Questao 24
 inserir :: Int -> [Int] -> [Int]
 inserir n [] = [n]
-inserir n (x:xs) | x > n = [n, x] ++ xs
+inserir n (x:xs)
+    | x > n = [n, x] ++ xs
     | otherwise = x:inserir n xs
 
 -- Questao 25
 isSorted :: [Int] -> Bool
 isSorted [] = True
-isSorted (x:y:xs) | x < y && length xs > 0 = True && isSorted (y:xs)
+isSorted (x:y:xs)
+    | x < y && not (null xs) = True && isSorted (y:xs)
     | x < y = True
     | otherwise = False
+
+-- Questao 26
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort (x:xs) = qsort [y | y <- xs, y < x] ++ [x] ++ qsort [y | y <- xs, y >= x]
 
 -- Questao 27
 rotEsq :: Int -> [a] -> [a]
 rotEsq 0 u = u
-rotEsq n (x:xs) | n == 1 = xs ++ [x]
+rotEsq n (x:xs)
+    | n == 1 = xs ++ [x]
     | otherwise = rotEsq (n-1) $ xs ++ [x]
 
 -- Questao 28
@@ -195,6 +213,19 @@ primo n = length [x | x <- [1..n], mod n x == 0] == 2
 sdig :: Int -> Int
 sdig 0 = 0
 sdig n = mod n 10 + sdig (div n 10)
+
+-- Questao 36
+cont _ [] = 0
+cont x (y:ys)
+    | x == y = 1 + cont x ys
+    | otherwise = 0 
+
+compac :: [Int] -> [[Int]]
+compac [] = []
+compac (x:xs)
+    | tes == 0 = [x]:compac xs
+    | otherwise = [tes+1, x]:compac (drop tes xs)
+    where tes = cont x xs
 
 -- Questao 37
 splitints :: [Int] -> ([Int], [Int])
